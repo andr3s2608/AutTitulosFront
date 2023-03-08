82f39885828f3af16d5f5b02efa1bfef320fcb59
Jean Lozano <jmlozano@soaint.com>
2023-03-08 17:04:26 -0500

    popup init citizen

--- src/app/feature/request/pages/user-request/user-request.component.ts
+++ src/app/feature/request/pages/user-request/user-request.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import Swal from "sweetalert2";
+import {ArchiveService} from "../../../../core/services/archive.service";
 
 /**
  * Componente que moldea la página de la solicitud del ciudadano
@@ -8,6 +10,45 @@ import { Component } from '@angular/core';
   templateUrl: './user-request.component.html',
   styleUrls: ['./user-request.component.scss']
 })
-export class UserRequestComponent {
+export class UserRequestComponent implements OnInit {
+
+  /**
+   * Ruta de la imagen del popup inicial
+   */
+  private readonly rutaImagenPopUpInicial: string;
+
+  /**
+   * Ruta del pdf con el listado de instituciones
+   */
+  private readonly rutaPdfListadoInstituciones: string;
+
+  constructor(private archiveService: ArchiveService) {
+    this.rutaImagenPopUpInicial = './assets/images/infografia-popup-inicial.jpg'
+    this.rutaPdfListadoInstituciones = './assets/binaries/listado.pdf'
+  }
+
+
+  ngOnInit(): void {
+    this.popUpInicial();
+  }
+
+
+  public popUpInicial(): void {
+    Swal.fire({
+      title: 'Información importante',
+      showCloseButton: true,
+      confirmButtonText: 'Cerrar',
+      confirmButtonColor: '#3366CC',
+      showDenyButton: true,
+      denyButtonText: 'Ver listado',
+      denyButtonColor: '#3366CC',
+      html: `<img alt='registro-titulos' src='${this.rutaImagenPopUpInicial}' style="width: 100%;">`,
+      width: '60%',
+    }).then( result => {
+      if (result.isDenied) {
+        this.archiveService.downloadArchive(this.rutaPdfListadoInstituciones,'listadoInstituciones', '.pdf');
+      }
+    });
+  }
 
 }
