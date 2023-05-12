3aadc29f4725a607c490c9402f99b80937c8005b
Jean Lozano <jmlozano@soaint.com>
2023-05-12 17:25:27 -0500

    fix professionalCard

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,9 +7,9 @@ export const environment = {
   PROCEDURE_SHARED_URI: 'https://apm-aeu-sds-dev-shared.azure-api.net/tramites-shared/api',
   PROCEDURE_SECURITY_URI: 'https://apm-aeu-sds-dev-shared.azure-api.net/security/api/v2/Security',
   PROCEDURE_NOTIFICATIONS_URI: 'https://apm-aeu-sds-dev-shared.azure-api.net/notifications/api/v1',
-  PROCEDURE_LOCAL_URI:'http://localhost:8080/api/v1',
+  //PROCEDURE_LOCAL_URI:'http://localhost:8080/api/v1',
   //PROCEDURE_LOCAL_URI:'https://localhost:5001/api/v1',
-  //PROCEDURE_LOCAL_URI:'http://back-tramite-aut-titulos-des-sds-tramite19-titulosensalud.apps.openshiftdev.soain.lcl/api/v1',
+  PROCEDURE_LOCAL_URI:'http://back-tramite-aut-titulos-des-sds-tramite19-titulosensalud.apps.openshiftdev.soain.lcl/api/v1',
   PROCEDURE_BLOB_URI:'https://apm-aeu-sds-dev-shared.azure-api.net/filemanager/api/v1',
 
 };
